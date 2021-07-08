014ab0fdd56242d0aef922f4baeb70643b84a83e
Román Martínez <roman.mx3@gmail.com>
2021-07-08 17:17:28 -0500

    Update intro comments

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,36 @@
 /*
 
-Typescript es un superset de Javascript
-Establece "contratos" usando "tipos"
+Typescript is a superset of Javascript, so you can include .ts modules into JS and
+vice versa. It establishes a "contract" between the members in your code to enforce
+strict type checking and improve the safety and reliability of your code.
 
-1. Instalar typescript
-npm i --save-dev typescript
+(optional) Installing typescript globally
+$ npm i -g typescript
 
-2. Running typescript
-Local version (inside node_modules/.bin/)
-include in "scripts" inside package.json, e.g. "dev: tsc"
+Two commands become available:
+$ tsc           // convert .ts files into .js
+$ tsserver      // ???
+
+1. Initializing a typescript project
+$ npm init [-y]
+$ npm i --save-dev typescript
+
+2. Create a config file with standard defaults:
+$ tsc --init
+- Reference: https://www.typescriptlang.org/tsconfig
+
+3. Running typescript
+Local version:
+- Include a command including "tsc" inside the "scripts" property in package.json, e.g:
+- package.json "scripts": { "dev": "tsc", "watch": "tsc --watch" }
 $ npm run dev
+$ npm run watch
 
-Global version
-Just use directly:
+Global version:
+- Just use 'tsc' directly:
 $ tsc
 
-3. Before running a config file for TS is required, create it:
-$ tsc --init
+*/
 
 
 */
