1780801ceaded7ef881e372aa3a8bfb4467a8a9a
Román Martínez <roman.mx3@gmail.com>
2021-07-08 11:15:38 -0500

    Create index.ts

---
+++ index.js
@@ -0,0 +1,24 @@
+"use strict";
+/*
+
+Typescript es un superset de Javascript
+Establece "contratos" usando "tipos"
+
+1. Instalar typescript
+npm i --save-dev typescript
+
+2. Running typescript
+Local version (inside node_modules/.bin/)
+include in "scripts" inside package.json, e.g. "dev: tsc"
+$ npm run dev
+
+Global version
+Just use directly:
+$ tsc
+
+3. Before running a config file for TS is required, create it:
+$ tsc --init
+
+
+*/
+console.log('trololo 456!');
