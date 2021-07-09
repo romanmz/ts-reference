083370ee5a2f96e80b7886ed65b80c247cb686a0
Román Martínez <roman.mx3@gmail.com>
2021-07-09 16:19:03 -0500

    Added info about config file

---
+++ src/config-file.ts
@@ -0,0 +1,12 @@
+/*
+
+compilerOptions
+--------------------------------------------------
+
+rootDir         // input folder
+outDir          // output folder
+sourceMap       // if set to 'true', generates source maps for each generated .js file
+
+target          // ES version to transpile to
+
+*/
